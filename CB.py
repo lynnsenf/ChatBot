96a0335f7809dede7c23d80bb3c72d47eb080b83
lynnsenf <50361144+lynnsenf@users.noreply.github.com>
2019-11-20 20:27:52 +0100

    Add files via upload

---
+++ CB.py
@@ -0,0 +1,90 @@
+import random
+import string  # to process standard python strings
+
+import nltk
+
+nltk.download('punkt')  # first-time use only
+nltk.download('wordnet')  # first-time use only
+
+flag = True
+print("Hi there, I can be any of the six Friends, who do you want me to be?")
+friend = input()
+f = open('1.1.txt', 'r')
+roboname = friend
+raw = f.read()
+raw = raw.lower()  # converts to lowercase
+
+sent_tokens = nltk.sent_tokenize(raw)  # converts to list of sentences
+word_tokens = nltk.word_tokenize(raw)  # converts to list of words
+
+lemmer = nltk.stem.WordNetLemmatizer()
+
+
+# WordNet is a semantically-oriented dictionary of English included in NLTK.
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+
+
+remove_punkt_dict = dict((ord(punkt), None) for punkt in string.punctuation)
+
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punkt_dict)))
+
+
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey", "How you doin'?")
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me", "How you doin'?"]
+ROSS_QUOTES = ("PIVOT!", "We were on a break!", "Unagi!")
+MONICA_QUOTES = ("Seven!", "I have to clean.")
+RACHEL_QUOTES = ("Sorry I'm late, but I left late. ...",)
+PHOEBE_QUOTES = ("That is brand new information.", "She's your lobster. ...")
+CHANDLER_QUOTES = ("Tell him to email me at www-dot-haha-not-so-much-dot-com!")
+JOEY_QUOTES = ("How you doin'?", "Va' fa Napoli!", "Here come the meat sweats...")
+
+
+def greeting(sentence):
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def response(user_response):
+    robo_response = ''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize)
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx = vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if req_tfidf == 0:
+        robo_response = robo_response + "I am sorry! I don't understand you"
+    else:
+        robo_response = robo_response + sent_tokens[idx]
+    return robo_response
+
+
+print(friend.upper() + ": Hi I am " + friend.upper() + "! " + random.choice(MONICA_QUOTES))
+while flag:
+    user_response = input()
+    user_response = user_response.lower()
+    if user_response != 'bye':
+        if user_response == 'thanks' or user_response == 'thank you':
+            flag = False
+            print(friend + ": You are welcome..")
+        else:
+            if greeting(user_response) is not None:
+                print(friend + ": " + greeting(user_response))
+            else:
+                print(friend + ': ')
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag = False
+        print(+": Bye! take care..")
---
+++ help.py
@@ -0,0 +1,18 @@
+if friend_choice == 'rachel':
+    f = open('rachel.txt', 'r')
+    friend = 'rachel'
+elif friend_choice == 'chandler':
+    f = open('chandler.txt', 'r')
+    friend = 'chandler'
+elif friend_choice == 'monica':
+    f = open('monica.txt', 'r')
+    friend = 'monica'
+elif friend_choice == 'ross':
+    f = open('ross.txt', 'r')
+    friend = 'ross'
+elif friend_choice == 'joey':
+    f = open('joey.txt', 'r')
+    friend = 'joey'
+elif friend_choice == 'phoebe':
+    f = open('phoebe.txt', 'r')
+    friend = 'phoebe'
