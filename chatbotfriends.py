9e742e976fc7c60eecec97c16ccb98dde931d7a3
knattarina <katharina.knappmann@gmail.com>
2019-11-26 15:05:22 +0100

    testest hothothot

--- chatbotfriends.py
+++ chatbotfriends.py
@@ -1,16 +1,15 @@
 import random
 import string  # to process standard python strings
-
 import nltk
 
-nltk.download('punkt')  # first-time use only
-nltk.download('wordnet')  # first-time use only
+#nltk.download('punkt')  # first-time use only
+#nltk.download('wordnet')  # first-time use only
 
 flag = True
 print("Hi there, I can be any of the six Friends, who do you want me to be?")
 friend = input()
-f = open('1.1.txt', 'r')
-roboname = friend
+
+f = open(friend + '.txt', 'r')
 raw = f.read()
 raw = raw.lower()  # converts to lowercase
 
@@ -27,20 +26,24 @@ def LemTokens(tokens):
 
 remove_punkt_dict = dict((ord(punkt), None) for punkt in string.punctuation)
 
-
 def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punkt_dict)))
 
 
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey", "How you doin'?")
 GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me", "How you doin'?"]
-ROSS_QUOTES = ("PIVOT!", "We were on a break!", "Unagi!")
-MONICA_QUOTES = ("Seven!", "I have to clean.")
-RACHEL_QUOTES = ("Sorry I'm late, but I left late. ...",)
-PHOEBE_QUOTES = ("That is brand new information.", "She's your lobster. ...")
-CHANDLER_QUOTES = ("Tell him to email me at www-dot-haha-not-so-much-dot-com!")
-JOEY_QUOTES = ("How you doin'?", "Va' fa Napoli!", "Here come the meat sweats...")
-
+if friend == "Ross":
+    FRIEND_QUOTES = ("PIVOT!", "We were on a break!", "Unagi!")
+elif friend == "Monica":
+    FRIEND_QUOTES = ("Seven!", "I have to clean.")
+elif friend == "Rachel":
+    FRIEND_QUOTES = ("Sorry I'm late, but I left late. ...","")
+elif friend == "Phoebe":
+    FRIEND_QUOTES = ("That is brand new information.", "She's your lobster. ...")
+elif friend == "Chandler":
+    FRIEND_QUOTES = ("Tell him to email me at www-dot-haha-not-so-much-dot-com!","")
+elif friend == "Joey":
+    FRIEND_QUOTES = ("How you doin'?", "Va' fa Napoli!", "Here come the meat sweats...")
 
 def greeting(sentence):
     for word in sentence.split():
@@ -70,7 +73,7 @@ def response(user_response):
     return robo_response
 
 
-print(friend.upper() + ": Hi I am " + friend.upper() + "! " + random.choice(MONICA_QUOTES))
+print(friend.upper() + ": Hi I am " + friend.upper() + "! " + random.choice(FRIEND_QUOTES))
 while flag:
     user_response = input()
     user_response = user_response.lower()
@@ -82,9 +85,9 @@ while flag:
             if greeting(user_response) is not None:
                 print(friend + ": " + greeting(user_response))
             else:
-                print(friend + ': ')
-                print(response(user_response))
+
+                print(friend + ":" + response(user_response))
                 sent_tokens.remove(user_response)
     else:
         flag = False
-        print(+": Bye! take care..")
+        print(friend + ": Bye! take care..")
